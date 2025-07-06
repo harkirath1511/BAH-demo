8729fb672a74cb214bc9245a6517f1904c22557b
Harkirat Singh <singharkirath1511@gmail.com>
2025-07-06 21:51:23 +0530

    commit3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Compiler from './components/Compiler.jsx'
-import Upload from './pages/Upload.jsx'
-import Registry from './pages/Registry.jsx'
-import Compare from './pages/Compare.jsx'
-import About from './pages/About.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Compiler from './components/Compiler.jsx';
+import Upload from './pages/Upload.jsx';
+import Registry from './pages/Registry.jsx';
+import Compare from './pages/Compare.jsx';
+import About from './pages/About.jsx';
 // import Visualize from './pages/Visualize.jsx'
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' Component={Compiler}>
-          <Route path='home' Component={Home}></Route>
-          <Route path='upload' Component={Upload}></Route>
-          <Route path='registry' Component={Registry}></Route>
-          <Route path='compare' Component={Compare}></Route>
-          <Route path='about' Component={About}></Route>
-          {/* <Route path='visualize' Component={Visualize}></Route> */}
-
+        <Route path='/' element={<Compiler />}>
+          {/* Redirect / to /home */}
+          <Route index element={<Home />} />
+          <Route path='home' element={<Home />} />
+          <Route path='upload' element={<Upload />} />
+          <Route path='registry' element={<Registry />} />
+          <Route path='compare' element={<Compare />} />
+          <Route path='about' element={<About />} />
+          {/* <Route path='visualize' element={<Visualize />} /> */}
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
